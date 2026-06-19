4308c906d1236c19a67c18edfd338595c963a735
Juan Mendes <juansmends@gmail.com>
2026-06-19 19:20:11 -0300

    organização do arquivo main.py

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
+import logging
 from src.config import Settings
 from src.contacts import buscar_contato
-import logging
+from src.messaging import enviar_mensagem
 
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(levelname)s - %(message)s",
 )
 
+logger = logging.getLogger(__name__)
+
 settings = Settings.le_env()
 
 contatos = buscar_contato(settings=settings)
 
+enviadas = 0
+
 for contato in contatos:
-    print(contato["name"], contato["number"])
+    mensagem = f"Olá, {contato['name']} tudo bem com você?"
+    if enviar_mensagem(settings=settings, telefone=contato["number"], mensagem=mensagem):
+        enviadas += 1
+
+logger.info(f"Mensagens enviadas: {enviadas}")
